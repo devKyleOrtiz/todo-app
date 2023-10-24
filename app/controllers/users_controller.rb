8e6433f07197fe91eccdca884ca4476a2495fec7
Kyle <kyle@kyleo.dev>
2023-10-23 21:47:03 -0500

    Revert "working password encryption on backend, tested with postman"
    This reverts commit c220e928bee683dcfb4a8aaecfd251af8f5a83bc.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,5 @@
 class UsersController < ApplicationController
-     def create
-          @user = User.create(user_params)
-          render json: @user, status: :created
-     end
-
-     private
-
-     def user_params
-          params.require(:user).permit(:FIRST_NAME, :EMAIL, :password)
+     def show
+          
      end
 end
